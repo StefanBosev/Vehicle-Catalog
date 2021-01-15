a72eab5f436a322da8ed513664e58e6f7d8f6b4b
stellakasabova <stella.kasabova@gmail.com>
2021-01-15 23:12:35 +0200

    Added login page UI + GET/POST(wip) methods

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if login.method == 'GET':
+        return render_template('login.html')
+    elif login.method == 'POST':
+        return redirect('/homepage.html')
+
+@app.route('/login_button')
+def login_button():
+    return render_template('homepage.html')
+
 if __name__ == '__main__':
     app.run()
