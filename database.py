235d5d85ef7c0c6fba28c8ebe92ee23233983b4b
stellakasabova <stella.kasabova@gmail.com>
2021-01-17 13:13:30 +0200

    Changed from SQLite to MySQL

--- database.py
+++ database.py
@@ -1,36 +1,53 @@
-import sqlite3
+import mysql.connector
 
-DB_NAME = 'vehicle_catalog.db'
+DB_NAME = 'vehicle_catalog'
 
-connection = sqlite3.connect(DB_NAME)
+connection = mysql.connector.connect(
+  host="127.0.0.1",
+  database= DB_NAME
+)
 
 
-connection.cursor().execute('''
-    CREATE TABLE IF NOT EXISTS Users 
+connection.cursor().execute(
+    '''CREATE TABLE Users 
     (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-    )
-''')
+        user_id int primary key auto_increment,
+        username varchar(20) unique not null,
+        password varchar(50) not null
+    )'''
+)
 
-connection.cursor().execute('''
-    CREATE TABLE IF NOT EXISTS Vehicles
+connection.cursor().execute(
+    '''CREATE TABLE Vehicles
     (
-        vehicle_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        owner_id INTEGER NOT NULL,
-        model TEXT NOT NULL,
-        colour TEXT NOT NULL,
-        manufacture_year DATE NOT NULL,
+        vehicle_id int primary key auto_increment,
+        owner_id int not null,
+        model varchar(40) not null,
+        colour varchar(20) not null,
+        manufacture_year date not null,
         FOREIGN KEY(owner_id) REFERENCES Users(user_id)
-    )
-''')
+    )'''
+)
+
+connection.cursor().execute(
+    '''CREATE TABLE Services
+    (
+        service_id int primary key auto_increment,
+        vehicle_id int not null,
+        services enum('change oil', 'change tires', 'test breaks'),
+        service_date date not null,
+        FOREIGN KEY(vehicle_id) REFERENCES Vehicles(vehicle_id)
+    )'''
+)
 
 connection.commit()
 
 class DB:
     def __enter__(self):
-        self.connection = sqlite3.connect(DB_NAME)
+        self.connection = mysql.connector.connect(
+                            host="127.0.0.1",
+                            database= DB_NAME
+                        )
         return self.connection.cursor()
 
     def __exit__(self, type, value, traceback):
