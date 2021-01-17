eaeee218757d1387474965c304d640d26fffb92b
stellakasabova <stella.kasabova@gmail.com>
2021-01-17 11:46:03 +0200

    Moved CSS to HTML file + updated post method

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 
+from user import User
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if login.method == 'GET':
-        return render_template('login.html')
-    elif login.method == 'POST':
-        return redirect('/homepage.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if register.method == 'GET':
+        return render_template('register.html')
+    elif register.method == 'POST':
+        values = (
+            None,
+            request.form['username'],
+            User.hash_password(request.form['password'])
+        )
+        User(*values).create()
 
-@app.route('/login_button')
-def login_button():
+        return redirect('/')
+
+@app.route('/register_button')
+def register_button():
     return render_template('homepage.html')
 
 if __name__ == '__main__':
