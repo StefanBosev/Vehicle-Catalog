a63493669e92e2efcc202e7d7dabccc015d141fd
stellakasabova <stella.kasabova@gmail.com>
2021-01-18 03:27:08 +0200

    Added services(wip) and vehicle view page, main login is still incomplete

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import render_template, request, redirect, url_for
 from flask_httpauth import HTTPBasicAuth
 
 from user import User
+from vehicle import Vehicle
 
 app = Flask(__name__)
 
@@ -30,13 +31,26 @@ def register():
 def register_button():
     return render_template('homepage.html')
 
+@app.route('/login_button')
+def login_button():
+    return render_template('homepage.html')
+
 @app.route('/home_button')
 def home_button():
     return render_template('homepage.html')
 
 @app.route('/services_button')
 def services_button():
-    return render_template('homepage.html')
+    return render_template('services.html')
+
+@app.route('/view_my_cars_button')
+def view_cars():
+    return render_template('view_vehicles.html', vehicle = Vehicle.find_by_owner())
+
+@app.route('/view_my_cars_button/<int:vehicle_id>')
+def show_vehicle(vehicle_id):
+    vehicle = Vehicle.find_by_id(vehicle_id)
+    return render_template('view_vehicles.html', vehicle = vehicle)
 
 if __name__ == '__main__':
     app.run()
