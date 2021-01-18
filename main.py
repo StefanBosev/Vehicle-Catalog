35a4967b119b6b4d767e94d5a5fc0c783a2a2b23
stellakasabova <stella.kasabova@gmail.com>
2021-01-18 16:53:19 +0200

    UI for edit and create vehicle, logic semi-working in main

--- main.py
+++ main.py
@@ -45,19 +45,19 @@ def services_button():
 
 @app.route('/view_my_vehicles_button')
 def view_vehicles():
+    #owner_id from login
     return render_template('view_vehicles.html', vehicle = Vehicle.find_by_owner())
 
-@app.route('/view_my_vehicles_button/<int:vehicle_id>')
+@app.route('/view_my_vehicles/<int:vehicle_id>')
 def show_vehicle(vehicle_id):
     vehicle = Vehicle.find_by_id(vehicle_id)
     return render_template('view_vehicles.html', vehicle = vehicle)
 
-@app.route('/view_my_vehicles_button/<int:vehicle_id>/edit', methods=['GET', 'POST'])
+@app.route('/view_my_vehicles/<int:vehicle_id>/edit', methods=['GET', 'POST'])
 def edit_vehicle(vehicle_id):
     vehicle = Vehicle.find_by_id(vehicle_id)
     if request.method == 'GET':
-
-        return render_template('edit_post.html', vehicle = vehicle)
+        return render_template('edit_vehicle.html', vehicle = vehicle)
     elif request.method == 'POST':
         vehicle.model = request.form['model']
         vehicle.colour = request.form['colour']
@@ -66,7 +66,17 @@ def edit_vehicle(vehicle_id):
 
         return redirect(url_for('show_vehicles', vehicle_id=vehicle.vehicle_id))
 
-@app.route('/view_my_vehicles_button/<int:vehicle_id>/delete', methods=['POST'])
+@app.route('/view_vehicles/new', methods=['GET', 'POST'])
+def add_vehicle():
+    if request.method == 'GET':
+        return render_template('new_vehicle.html')
+    elif request.method == 'POST':
+        #owner_id should be gotten from login?
+        values = (None, request.form['owner_id'], request.form['model'], request.form['colour'], request.form['manufacture_year'])
+        Vehicle(*values).create()
+        return redirect(url_for('view_vehicles'))
+
+@app.route('/view_my_vehicles/<int:vehicle_id>/delete', methods=['POST'])
 def delete_vehicle(vehicle_id):
     vehicle = Vehicle.find_by_id(vehicle_id)
     vehicle.delete()
